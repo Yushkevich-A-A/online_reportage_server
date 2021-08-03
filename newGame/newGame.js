6185cd0c5d1afa254c799957646f9d3abb562782
Yushkevich-A-A <Mortifer1993@yandex.ru>
2021-08-03 19:03:48 +0300

    server complite

--- newGame/newGame.js
+++ newGame/newGame.js
@@ -8,6 +8,7 @@ class NewGame {
 
   createNewGame() {
     eventSubscription.add({type: null, message:'Игра началась'});
+    const timeFirstEvent = Math.round(Math.random() * 300) * 1000;
 
     this.timer = setTimeout(function matchEvent() {
       if (eventSubscription.data.length === 50) {
@@ -17,29 +18,22 @@ class NewGame {
       }
 
       const percent = Math.round(Math.random() * 10);
-      const nextTimer = Math.round(Math.random() * 20) * 1000;
+      const nextTimer = Math.round(Math.random() * 300) * 1000;
     
       let item = null;
-      if (percent >= 5) {
+      if (percent <= 5) {
         item = matchEvents.data[2];
-      } else if (percent >= 2) {
+      } else if (percent <= 8) {
         item = matchEvents.data[1];
       } else {
         item = matchEvents.data[0];
       }
     
-      item.date = Date.now();
+      item.date = +new Date();
       eventSubscription.add(item);
       this.timer = setTimeout(matchEvent, nextTimer);
-    }, 5000);
+    }, timeFirstEvent);
     
-  }
-
-  resetGame() {
-    setTimeout(() => {
-      eventSubscription.lenght = '';
-      this.createNewGame();
-    })
   }
 }
 
